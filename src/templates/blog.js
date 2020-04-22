58a567485fb8d1d1630f5f49076107de6a5f127f
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2020-04-22 14:35:51 -0700

    fix: documentToReactComponents args

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -34,7 +34,7 @@ const Blog = (props) => {
           <Head title={props.data.contentfulBlogPost.title} />
           <h1>{props.data.contentfulBlogPost.title}</h1>
           <p>{props.data.contentfulBlogPost.publishedDate}</p>
-          {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
+          {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
         </Layout>
     )
 }
