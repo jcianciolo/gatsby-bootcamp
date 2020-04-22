51861c1279ded270c6290e4d99fb9b23681c54bd
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2020-04-22 16:14:59 -0700

    update: removed link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home" />
             <h1>Hello.</h1>
             <h2>I'm John, a full-stack developer living in the Bay Area.</h2>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </Layout>
     )
 }
